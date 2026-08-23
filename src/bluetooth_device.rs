4a7108d2432b6c649546a42398867ff078b99eb7
thegecko <rob.moran@arm.com>
2026-08-23 15:33:02 +0100

    Fiddling

--- src/bluetooth_device.rs
+++ src/bluetooth_device.rs
@@ -1,13 +1,112 @@
 use btleplug::api::Peripheral as _;
 use btleplug::platform;
+use napi::bindgen_prelude::Uint8Array;
 use napi_derive::napi;
+
+#[napi]
+pub struct BluetoothCharacteristicProperties {
+    #[napi(writable = false)]
+    pub broadcast: bool,
+    #[napi(writable = false)]
+    pub read: bool,
+    #[napi(writable = false)]
+    pub writeWithoutResponse: bool,
+    #[napi(writable = false)]
+    pub write: bool,
+    #[napi(writable = false)]
+    pub notify: bool,
+    #[napi(writable = false)]
+    pub indicate: bool,
+    #[napi(writable = false)]
+    pub authenticatedSignedWrites: bool,
+    #[napi(writable = false)]
+    pub reliableWrite: bool,
+    #[napi(writable = false)]
+    pub writableAuxiliaries: bool,
+}
+
+#[napi]
+pub struct BluetoothRemoteGATTDescriptor {
+    #[napi(writable = false)]
+    pub uuid: String, // UUID
+    #[napi(writable = false)]
+    pub value: Option<Uint8Array>,
+}
+
+impl BluetoothRemoteGATTDescriptor {
+    /*
+      Promise<DataView> readValue();
+      Promise<undefined> writeValue(BufferSource value);
+    */
+}
+
+#[napi]
+pub struct BluetoothRemoteGATTCharacteristic {
+    #[napi(writable = false)]
+    pub uuid: String, // UUID
+    #[napi(writable = false)]
+    pub value: Option<Uint8Array>,
+}
+
+impl BluetoothRemoteGATTCharacteristic {
+    /*
+      Promise<BluetoothRemoteGATTDescriptor> getDescriptor(BluetoothDescriptorUUID descriptor);
+      Promise<sequence<BluetoothRemoteGATTDescriptor>>
+        getDescriptors(optional BluetoothDescriptorUUID descriptor);
+      Promise<DataView> readValue();
+      Promise<undefined> writeValue(BufferSource value);
+      Promise<undefined> writeValueWithResponse(BufferSource value);
+      Promise<undefined> writeValueWithoutResponse(BufferSource value);
+      Promise<BluetoothRemoteGATTCharacteristic> startNotifications();
+      Promise<BluetoothRemoteGATTCharacteristic> stopNotifications();
+    */
+}
+
+#[napi]
+pub struct BluetoothRemoteGATTService {
+    #[napi(writable = false)]
+    pub uuid: String, // UUID
+    #[napi(writable = false)]
+    pub isPrimary: bool,
+}
+
+impl BluetoothRemoteGATTService {
+    /*
+      Promise<BluetoothRemoteGATTCharacteristic>
+        getCharacteristic(BluetoothCharacteristicUUID characteristic);
+      Promise<sequence<BluetoothRemoteGATTCharacteristic>>
+        getCharacteristics(optional BluetoothCharacteristicUUID characteristic);
+      Promise<BluetoothRemoteGATTService>
+        getIncludedService(BluetoothServiceUUID service);
+      Promise<sequence<BluetoothRemoteGATTService>>
+        getIncludedServices(optional BluetoothServiceUUID service);
+    */
+}
+
+#[napi]
+pub struct BluetoothRemoteGATTServer {
+    #[napi(writable = false)]
+    pub connected: bool,
+}
+
+impl BluetoothRemoteGATTServer {
+    /*
+      Promise<BluetoothRemoteGATTServer> connect();
+      undefined disconnect();
+      Promise<BluetoothRemoteGATTService> getPrimaryService(BluetoothServiceUUID service);
+      Promise<sequence<BluetoothRemoteGATTService>>
+        getPrimaryServices(optional BluetoothServiceUUID service);
+    */
+}
 
 #[napi]
 pub struct BluetoothDevice {
     #[napi(writable = false)]
     pub id: String,
     #[napi(writable = false)]
-    pub name: String,
+    pub name: Option<String>,
+    #[napi(writable = false)]
+    pub watchingAdvertisements: bool,
 }
 
 impl BluetoothDevice {
@@ -22,7 +121,13 @@ impl BluetoothDevice {
 
         Self {
             id: peripheral.id().to_string(),
-            name,
+            name: Some(name),
+            watchingAdvertisements: false,
         }
     }
+    /*
+        Promise<undefined> forget();
+    Promise<undefined> watchAdvertisements(
+        optional WatchAdvertisementsOptions options = {});
+        */
 }
--- tsc/index.ts
+++ tsc/index.ts
@@ -1,62 +1,187 @@
-import { nativeGetDevices, BluetoothDevice } from '../index.js'
+import { BluetoothDevice as NativeBluetoothDevice, nativeGetDevices } from '../index.js';
 
 /**
  * Bluetooth Options interface
  */
 interface BluetoothOptions {
     /**
-     * A `device found` callback function to allow the user to select a device
+     * A `device found` callback function to allow the user to select a device.
      */
-    deviceFound?: (device: BluetoothDevice, selectFn: () => void) => boolean;
+    deviceFound?: (device: NativeBluetoothDevice, selectFn: () => void) => boolean;
 
     /**
-     * The amount of seconds to scan for the device (default is 10)
+     * The amount of seconds to scan for the device.
      */
     scanTime?: number;
 
     /**
-     * Optional flag to automatically allow all devices
+     * Optional flag to automatically allow all devices.
      */
     allowAllDevices?: boolean;
 
     /**
-     * An optional referring device
+     * An optional referring device.
      */
-    referringDevice?: BluetoothDevice;
+    referringDevice?: NativeBluetoothDevice;
 
     /**
-     * An optional index of bluetooth adapter to use
+     * An optional index of bluetooth adapter to use.
      */
     adapterIndex?: number;
 }
 
-class Bluetooth extends EventTarget implements Partial<Bluetooth> {
+class Bluetooth extends EventTarget {
+    public readonly referringDevice?: NativeBluetoothDevice;
 
-    public async loadDevices(): Promise<BluetoothDevice[]> {
-        let devices = await nativeGetDevices();
-        return devices;
+    private readonly allowAllDevices: boolean;
+    private readonly deviceFound?: (device: NativeBluetoothDevice, selectFn: () => void) => boolean;
+    private readonly allowedDevices = new Set<string>();
+
+    constructor(options: BluetoothOptions = {}) {
+        super();
+
+        this.allowAllDevices = options.allowAllDevices ?? false;
+        this.deviceFound = options.deviceFound;
+        this.referringDevice = options.referringDevice;
+
+        void options.scanTime;
+        void options.adapterIndex;
+    }
+
+    /**
+     * Gets the availability of a bluetooth adapter.
+     */
+    public getAvailability(): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+
+    /**
+     * Scans for a device matching optional filters.
+     */
+    public async requestDevice(options: RequestDeviceOptions = { filters: [] }): Promise<NativeBluetoothDevice> {
+        this.validateRequestOptions(options);
+
+        const devices = await nativeGetDevices();
+        const matchedDevice = devices.find(device => this.matchesRequestOptions(device, options));
+
+        if (!matchedDevice) {
+            throw new Error('requestDevice error: no devices found');
+        }
+
+        let selected = true;
+        const selectFn = () => {
+            selected = true;
+        };
+
+        if (this.deviceFound) {
+            selected = this.deviceFound(matchedDevice, selectFn) === true;
+        }
+
+        if (!selected) {
+            throw new Error('requestDevice error: device not selected');
+        }
+
+        this.allowedDevices.add(matchedDevice.id);
+        return matchedDevice;
+    }
+
+    /**
+     * Get all allowed bluetooth devices.
+     */
+    public async getDevices(): Promise<NativeBluetoothDevice[]> {
+        const devices = await nativeGetDevices();
+
+        if (this.allowAllDevices) {
+            return devices;
+        }
+
+        return devices.filter(device => this.allowedDevices.has(device.id));
+    }
+
+    public loadDevices(): Promise<NativeBluetoothDevice[]> {
+        return nativeGetDevices();
+    }
+
+    /**
+     * Request LE scan.
+     */
+    public requestLEScan(_options?: BluetoothLEScanOptions): Promise<BluetoothLEScan> {
+        throw new Error('requestLEScan error: method not implemented.');
+    }
+
+    private validateRequestOptions(options: RequestDeviceOptions): void {
+        if ('filters' in options && options.filters !== undefined) {
+            if (options.filters.length === 0) {
+                throw new TypeError('requestDevice error: no filters specified');
+            }
+
+            const hasEmptyFilter = options.filters.some(filter => Object.keys(filter).length === 0);
+            if (hasEmptyFilter) {
+                throw new TypeError('requestDevice error: empty filter specified');
+            }
+
+            const hasEmptyNamePrefix = options.filters.some(filter => filter.namePrefix === '');
+            if (hasEmptyNamePrefix) {
+                throw new TypeError('requestDevice error: empty namePrefix specified');
+            }
+
+            return;
+        }
+
+        if ('acceptAllDevices' in options && options.acceptAllDevices === true) {
+            return;
+        }
+
+        throw new TypeError('requestDevice error: specify filters or acceptAllDevices');
+    }
+
+    private matchesRequestOptions(device: NativeBluetoothDevice, options: RequestDeviceOptions): boolean {
+        if ('acceptAllDevices' in options && options.acceptAllDevices === true) {
+            return true;
+        }
+
+        if (!('filters' in options) || options.filters === undefined) {
+            return false;
+        }
+
+        return options.filters.some(filter => this.matchesFilter(device, filter));
+    }
+
+    private matchesFilter(device: NativeBluetoothDevice, filter: BluetoothLEScanFilter): boolean {
+        const name = device.name ?? '';
+
+        if (filter.name !== undefined && filter.name !== name) {
+            return false;
+        }
+
+        if (filter.namePrefix !== undefined && !name.startsWith(filter.namePrefix)) {
+            return false;
+        }
+
+        return true;
     }
 }
 
 /**
-  * Default bluetooth instance synonymous with `navigator.bluetooth`
-  */
-const bluetooth = new Bluetooth();
+ * Default bluetooth object, allowing all devices by default.
+ */
+const bluetooth = new Bluetooth({
+    allowAllDevices: true,
+});
+
+/**
+ * Default WebBluetooth object, mimicking `navigator.bluetooth` when present.
+ */
+const webbluetooth = typeof navigator !== 'undefined' && navigator.bluetooth ? navigator.bluetooth : bluetooth;
 
 export {
-    // Default bluetooth object (mimics navigator.bluetooth)
     bluetooth,
-
-    // Main object class
+    webbluetooth,
     Bluetooth,
-
-    // Types
     BluetoothOptions,
-
-    // getAdapters
 };
 
 /**
- * Helper methods and enums
+ * Helper methods and enums.
  */
 export * from './uuid';
